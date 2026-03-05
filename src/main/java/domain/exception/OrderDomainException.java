f56cb9513ec198dab73700b5a168b3c0e05bd453
mateuszf222 <mateusz.fabich@gmail.com>
2026-03-05 12:51:07 +0100

    Domain layer with order model

---
+++ src/main/java/domain/exception/OrderDomainException.java
@@ -0,0 +1,6 @@
+package domain.exception;
+
+public class OrderDomainException extends RuntimeException {
+    public OrderDomainException(String message) { super(message); }
+    public OrderDomainException(String message, Throwable cause) { super(message, cause); }
+}
