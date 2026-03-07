a2bb62f223770df0c68b52e3ef3e74cf1da3b7d3
mateuszf222 <mateusz.fabich@gmail.com>
2026-03-07 10:10:40 +0100

    Inbound adapter as REST controller

---
+++ src/main/java/org/example/oms/infrastructure/adapter/in/web/CreateOrderRequest.java
@@ -0,0 +1,17 @@
+package org.example.oms.infrastructure.adapter.in.web;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public record CreateOrderRequest(
+        UUID customerId,
+        List<OrderItemRequest> items
+) {
+    public record OrderItemRequest(
+            UUID productId,
+            int quantity,
+            BigDecimal price,
+            String currency
+    ) {}
+}
