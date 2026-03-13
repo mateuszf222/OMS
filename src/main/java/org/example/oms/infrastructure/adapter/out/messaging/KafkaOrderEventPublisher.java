8e8282ef43bc173c9ce589d22a4e7594da6e0eab
mateuszf222 <mateusz.fabich@gmail.com>
2026-03-13 14:58:12 +0100

    Kafka adapter to stream events

--- src/main/java/org/example/oms/infrastructure/adapter/out/messaging/KafkaOrderEventPublisher.java
+++ src/main/java/org/example/oms/infrastructure/adapter/out/messaging/KafkaOrderEventPublisher.java
@@ -2,12 +2,27 @@ package org.example.oms.infrastructure.adapter.out.messaging;
 
 import org.example.oms.application.port.out.OrderEventPublisher;
 import org.example.oms.domain.model.Order;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaOrderEventPublisher implements OrderEventPublisher {
 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaOrderEventPublisher(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public void publishOrderCreatedEvent(Order order) {
+        OrderCreatedEvent event = new OrderCreatedEvent(
+                order.getId(),
+                order.getCustomerId(),
+                order.getTotalAmount().amount(),
+                order.getTotalAmount().currency().getCurrencyCode()
+        );
+
+        kafkaTemplate.send("order-events", order.getId().toString(), event);
     }
 }
